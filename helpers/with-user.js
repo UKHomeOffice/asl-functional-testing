3cc1f0c7ebc06632def274920d542ad190bc33ed
Leonard Martin <leonard.martin@gmail.com>
2019-09-06 15:06:49 +0100

    Reload keycloak if login screen fails to render

--- helpers/with-user.js
+++ helpers/with-user.js
@@ -1,5 +1,11 @@
 module.exports = settings => browser => username => {
   browser.url('/logout');
+  try {
+    browser.waitForVisible('[name=username]', 10000);
+  } catch (e) {
+    browser.refresh();
+    browser.waitForVisible('[name=username]', 10000);
+  }
   browser.setValue('[name=username]', username);
   if (!settings.users[username]) {
     throw new Error(`Could not find user: ${username}`);
