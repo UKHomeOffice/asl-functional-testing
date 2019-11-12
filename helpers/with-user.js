c74245bffc1e7beccd7cc6ff958193255d27be55
Joe Fitter <joefitter@hotmail.com>
2019-11-12 10:57:33 +0000

    check no error message after login before waiting for login

--- helpers/with-user.js
+++ helpers/with-user.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 module.exports = settings => browser => username => {
   username = username || settings.defaultUser;
   browser.url('/logout');
@@ -13,5 +15,6 @@ module.exports = settings => browser => username => {
   }
   browser.setValue('[name=password]', settings.users[username]);
   browser.click('[name=login]');
+  assert.ok(!browser.$('span=Invalid username or password.').isExisting(), 'Failed authentication');
   browser.waitForVisible('h1*=Hello', 60000);
 };
