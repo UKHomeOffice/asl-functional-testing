b7dec97d8752b0241204dcdcc52ab414e4e65c91
Leonard Martin <leonard.martin@gmail.com>
2020-05-29 09:57:19 +0100

    Delete everything and start clean

--- config/index.js
+++
@@ -1,8 +0,0 @@
-const defaults = require('./defaults');
-const local = require('./load-local-config');
-
-const env = process.env.DRONE_DEPLOY_TO || process.env.TEST_ENV || (process.env.CI ? 'dev' : 'local');
-
-const selenium = process.env.SELENIUM_HOST || (process.env.CI ? 'selenium' : 'localhost');
-
-exports.config = Object.assign(local(env, defaults), { host: selenium });
