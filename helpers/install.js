f2d17a68efd3f1c37ca719135fb0b3988454245c
Leonard Martin <leonard.martin@gmail.com>
2020-06-02 16:27:05 +0100

    Set implicit wait timeout

--- helpers/install.js
+++ helpers/install.js
@@ -4,6 +4,7 @@ const downloadFile = require('./download-file');
 const waitForSync = require('./wait-for-sync');
 
 module.exports = (config) => {
+  browser.setTimeout({ implicit: 3000 });
   browser.addCommand('withUser', withUser(config));
   browser.addCommand('downloadFile', downloadFile(config));
   browser.addCommand('waitForSync', waitForSync(config));
