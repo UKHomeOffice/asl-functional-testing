51be0363391106a997364dc086efeeb08f616c6a
Leonard Martin <leonard.martin@gmail.com>
2020-06-08 10:30:29 +0100

    Add "wait for success" helper
    Waits for one of the two variants of success message to be displayed, without minding which.

--- helpers/install.js
+++ helpers/install.js
@@ -2,12 +2,14 @@
 const withUser = require('./with-user');
 const downloadFile = require('./download-file');
 const waitForSync = require('./wait-for-sync');
+const waitForSuccess = require('./wait-for-success');
 const selectMany = require('./select-many');
 
 module.exports = (config) => {
-  browser.setTimeout({ implicit: 3000 });
+  browser.setTimeout({ implicit: 500 });
   browser.addCommand('withUser', withUser(config));
   browser.addCommand('downloadFile', downloadFile(config));
   browser.addCommand('waitForSync', waitForSync(config));
+  browser.addCommand('waitForSuccess', waitForSuccess(config));
   browser.addCommand('selectMany', selectMany(config));
 };
