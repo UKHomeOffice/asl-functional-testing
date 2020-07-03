0b74f4aff06aa3e8aec8828d5b07e6c5510c4889
Joe Fitter <joefitter@hotmail.com>
2020-07-03 17:29:48 +0100

    added helper for filling out autocomplete fields

--- helpers/install.js
+++ helpers/install.js
@@ -4,9 +4,11 @@ const downloadFile = require('./download-file');
 const waitForSync = require('./wait-for-sync');
 const waitForSuccess = require('./wait-for-success');
 const selectMany = require('./select-many');
+const autocomplete = require('./autocomplete');
 
 module.exports = (config) => {
   browser.setTimeout({ implicit: 500 });
+  browser.addCommand('autocomplete', autocomplete(config));
   browser.addCommand('withUser', withUser(config));
   browser.addCommand('downloadFile', downloadFile(config));
   browser.addCommand('waitForSync', waitForSync(config));
